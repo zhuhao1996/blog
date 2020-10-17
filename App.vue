d6da3139ace3f9abea162b32707f85877f556c36
朱浩 <70883219+zhuhao1996@users.noreply.github.com>
2020-10-17 12:20:30 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-link to="/">写博客</router-link>
+    <router-link to="/show">博客</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
